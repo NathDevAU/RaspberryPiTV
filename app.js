c644df93614ea517899eff8d3b666975e1cb0231
Steven Hickson <me@stevenhickson.com>
2016-11-28 22:12:33 +0000

    allow cors

--- app.js
+++ app.js
@@ -59,6 +59,28 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
+//Attempt to allow access control origin
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 //app.get('/public/*', function(req, res, next) {
 //    if (req.client.authorized) {
 //      next(); // call the next handler, which in this case is express.static
